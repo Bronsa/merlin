ea3037c186423114096c3d99728daf36663efaaa
Nicola Mometto <me@bronsa.net>
2018-07-10 10:40:56 +0100

    Use Unix.open_process instead of Unix.create_process

--- src/extend/extend_driver.ml
+++ src/extend/extend_driver.ml
@@ -7,7 +7,6 @@ type t = {
   capabilities: P.capabilities;
   stdin: out_channel;
   stdout: in_channel;
-  mutable pid: int;
 
   notify: string -> unit;
   debug: string -> unit;
@@ -16,23 +15,9 @@ type t = {
 exception Extension of string * string * string
 
 let run ?(notify=ignore) ?(debug=ignore) name =
-  let pstdin, stdin = Unix.pipe () in
-  let stdout, pstdout = Unix.pipe () in
-  Unix.set_close_on_exec pstdin;
-  Unix.set_close_on_exec stdin;
-  Unix.set_close_on_exec pstdout;
-  Unix.set_close_on_exec stdout;
-  let pid =
-    Unix.create_process
-      ("ocamlmerlin-" ^ name) [||]
-      pstdin pstdout Unix.stderr
-  in
-  Unix.close pstdout;
-  Unix.close pstdin;
-  let stdin  = Unix.out_channel_of_descr stdin in
-  let stdout = Unix.in_channel_of_descr stdout in
+  let (stdout, stdin) = Unix.open_process ("ocamlmerlin-" ^ name) in
   match Extend_main.Handshake.negotiate_driver name stdout stdin with
-  | capabilities -> {name; capabilities; stdin; stdout; pid; notify; debug}
+  | capabilities -> {name; capabilities; stdin; stdout; notify; debug}
   | exception exn ->
     close_out_noerr stdin;
     close_in_noerr stdout;
@@ -40,17 +25,11 @@ let run ?(notify=ignore) ?(debug=ignore) name =
 
 let stop t =
   close_out_noerr t.stdin;
-  close_in_noerr t.stdout;
-  if t.pid <> -1 then (
-    let _, _ = Unix.waitpid [] t.pid in
-    t.pid <- -1;
-  )
+  close_in_noerr t.stdout
 
 let capabilities t = t.capabilities
 
 let reader t request =
-  if t.pid = -1 then
-    invalid_arg "Extend_main.Driver.reader: extension is closed";
   output_value t.stdin (P.Reader_request request);
   flush t.stdin;
   let rec aux () =
